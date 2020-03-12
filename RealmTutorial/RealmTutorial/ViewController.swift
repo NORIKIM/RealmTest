f89793c22525d69b54360ae9b0e24034df332300
NORIKIM <jinagold.k@gmail.com>
2020-03-12 14:45:02 +0900

    Create)Realm Tutorial project

---
+++ RealmTutorial/RealmTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmTutorial
+//
+//  Created by 김지나 on 2020/03/12.
+//  Copyright © 2020 김지나. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
